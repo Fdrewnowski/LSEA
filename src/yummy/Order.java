f498e320b73fb0eebf1250cc2d5365961f712096
Fdrewnowski <f.drewnowski@wp.pl>
2020-04-02 13:14:09 +0200

    java dock comments in order

--- src/yummy/Order.java
+++ src/yummy/Order.java
@@ -9,21 +9,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ *Class <i>Order</i> is made to allow users making orders. Object <i>Order</i>
+ * contains information about company and user which were making deal, list of
+ * ordered products and total price for this products.
+ * Class <i>Order</i> following fields:
+ * double totalPrice total price of all products
+ * List of Product allOrderedItems list of all products on invoice
+ * String companyFrom company from which order was made
+ * String whoOrdered user nickname who made order
  * @author fdrew
  */
 public final class Order {
     
-    private double totalPrice;                 //total price from order 
+    private double totalPrice;                      //total price from order 
     private final List<Product> allOrderedItems;    //list of all selected products
-    private String companyFrom;                 //company in which user buys
-    private String whoOrdered;                  //nickname of user
+    private String companyFrom;                     //company in which user buys
+    private String whoOrdered;                      //nickname of user
     
-    //constructor - order is empty 
-    //example of overloading
-
     /**
-     *
+     *<b>Constructor</b> for class <i>Order</i>, gives null values for all fields. 
+     * Made mostly for testing, example of overloading.
      */
     public Order(){
         this.totalPrice=0;
@@ -32,13 +37,12 @@ public final class Order {
         this.whoOrdered = null;
     }
     
-    //constructor - when we arleady have all information for an order
-
     /**
-     *
-     * @param companyFrom
-     * @param whoOrdered
-     * @param listOfProducts
+     *<b>Constructor</b> for class <i>Order</i>, it needs following parameters 
+     * two string parameters describing object(company from, who ordered) and ArrayList of Products.
+     * @param companyFrom is name of company in which order was made 
+     * @param whoOrdered is nickname of user who made order
+     * @param listOfProducts is list of all products in order
      */
 
     public Order(String companyFrom, String whoOrdered, List<Product> listOfProducts){
@@ -52,8 +56,8 @@ public final class Order {
     //Hermetization OOP principle - Accessors 
 
     /**
-     *
-     * @return
+     *Method <b> getTotalPrice()</b> is an accessor in class <i>Order</i>.
+     * @return total price for an order in double format
      */
  
     public double getTotalPrice(){
@@ -61,24 +65,24 @@ public final class Order {
     }
     
     /**
-     *
-     * @return
+     *Method <b> getAllOrderedItems()</b> is an accessor in class <i>Order</i>.
+     * @return field allOrderedItems which is List of all products on order
      */
     public List<Product> getAllOrderedItems(){
         return this.allOrderedItems;
     }
     
     /**
-     *
-     * @return
+     *Method <b> getCompanyFrom()</b> is an accessor in class <i>Order</i>.
+     * @return name of the company from which specific user is ordering
      */
     public String getCompanyFrom(){
         return this.companyFrom;
     }   
     
     /**
-     *
-     * @return
+     *Method <b> getWhoOrdered()</b> is an accessor in class <i>Order</i>.
+     * @return field whoOrdered, so nickname of user who made order 
      */
     public String getWhoOrdered(){
         return this.whoOrdered;
@@ -87,7 +91,8 @@ public final class Order {
     //Hermetization OOP principle - Mutators
 
     /**
-     *
+     *Method <b> setTotalPrice()</b> is an mutator in class <i>Order</i>. It goes
+     * through list of product and sum their price to get total price.
      */
 
     public void setTotalPrice(){    //goes through all list and sum their price
@@ -99,16 +104,18 @@ public final class Order {
     }
     
     /**
-     *
-     * @param companyFrom
+     *Method <b>setCompanyFrom(String companyFrom)</b> is a mutator which set 
+     * name of the company from which Order was made.
+     * @param companyFrom name of the company in String format.
      */
     public void setCompanyFrom(String companyFrom){
         this.companyFrom = companyFrom;
     }   
     
     /**
-     *
-     * @param whoOrdered
+     *Method <b>setWhoOrdered(String whoOrdered)</b> is a mutator which set 
+     * name of the user who ordered.
+     * @param whoOrdered nickname of the user who order in String format
      */
     public void setWhoOrdered(String whoOrdered){
         this.whoOrdered = whoOrdered;
@@ -117,8 +124,9 @@ public final class Order {
     //Methods which help dealing with list add and remove element
 
     /**
-     *
-     * @param newAssortment
+     *Method <b> saveOrderedItem(Product newAssortment)</b> is a function which save new object
+     * <i>Product</i> to existing list on field allOrderedItems in class <i>Order</i>.
+     * @param newAssortment element which will be added to the list, type <i>Product</i>
      */
     
     public void saveOrderedItem(Product newAssortment){
@@ -126,15 +134,19 @@ public final class Order {
     }
 
     /**
-     *
-     * @param assortmentToRemove
+     *Method <b> removeOrderedItem(Product assortmentToRemove)</b> is a function which remove object
+     * <i>Product</i> from existing list on field allOrderedItems in class <i>Order</i>.
+     * @param assortmentToRemove element which will be removed from the list, type <i>Product</i>
      */
     public void removeOrderedItem(Product assortmentToRemove){
         this.allOrderedItems.remove(assortmentToRemove);
     }
     
     /**
-     *
+     *Method <b>printOrder()</b> ia a function, which prints into console all
+     * crucial information about order with proper comments. We get
+     * information about who made order, company from which order was made, total price
+     * and list of ordered products.
      */
     public void printOrder(){    //Prints description about product
         System.out.print("Information about Order: \n");
